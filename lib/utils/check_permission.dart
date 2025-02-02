05b79fb45678a22de0a8afd2fcfd287213109f49
SubramanyaKS <subramanyaks2205@gmail.com>
2025-02-02 22:56:14 +0530

    added utils

---
+++ lib/utils/check_permission.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> checkPermission(Permission permission, BuildContext context)async {
+  final status= await permission.request();
+  if(status.isGranted){
+    if (!context.mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Storage permission granted")));
+
+  }
+  else{
+    if (!context.mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Storage permission not granted")));
+  }
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,6 @@
+const title='PHRASEPICS';
+const tagline ='Transform Your Words into Stunning Images';
+const NOTHAVEACCOUNT="Don't have an account?";
+const HAVEACCOUNT="Already have an account? ";
+const LOGIN="LOGIN";
+const SIGNUP="SIGNUP";
